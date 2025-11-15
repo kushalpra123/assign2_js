65f17729927bcaf5f4614a0c7c335aa0f583127e
kushalpra123 <kushalpra49@gmail.com>
2025-11-15 02:53:03 -0500

    added a method to explain how the users smoothie is madeup of or looks like

--- script.js
+++ script.js
@@ -4,5 +4,14 @@ class SmoothieMachine {
         this.ingredients = ingredients;
         this.milk = milk;
 }
+//a function inside a class is called a method
+describe(){
+return `
+<h3>Your smoothie Order:</h3>
+<p>Size: ${this.size}</p>
 
+<p>Ingredients: ${this.ingredients.join(", ")}</p> 
+<p>Milk: ${this.milk}</p>
+`
+}
 }        
